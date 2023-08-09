22aef4d967b2983da09e97503169d5c60f10a006
MohamedSafwatAhmed <110623152+MohamedSafwatAhmed@users.noreply.github.com>
2023-08-10 00:51:45 +0300

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,16 @@
+#------count the number 4 in a given list---------#
+#x=[7,4,1,4,2,4,8,6,4]
+#n=x.count(4)
+#print(n)
+
+#-------python program to test whether a passes letter ia a vowel or not----#
+#def is_vowel(ch):
+#    vowels="aeiouAEIOU"
+#    return ch in vowels
+#print(is_vowel('b'))
+
+
+#-------Acesses the Environment variable and print path---------#
+#import os 
+#path_value=os.environ.get('PATH')
+#print(path_value)
