e427916d9154d05752336830e31124cf9d05ecdf
moham1250ed@gmail.com <moham1250ed@gmail.com>
2023-09-02 18:03:57 +0300

    initial

--- Python/Session_5/Task_4/Client_1.py
+++ Python/Session_5/Task_4/Client_1.py
@@ -1,18 +1,17 @@
 import threading
 import socket
-alias = input('Choose an alias >>> ')
+import random 
+print('Client')
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 59000))
+client.connect(('127.0.0.3', 59000))
 
 
 def client_receive():
     while True:
+  
         try:
             message = client.recv(1024).decode('utf-8')
-            if message == "alias?":
-                client.send(alias.encode('utf-8'))
-            else:
-                print(message)
+            print(message)
         except:
             print('Error!')
             client.close()
@@ -21,7 +20,7 @@ def client_receive():
 
 def client_send():
     while True:
-        message = f'{alias}: {input("")}'
+        message=str(random.randint(10,500))
         client.send(message.encode('utf-8'))
 
 
